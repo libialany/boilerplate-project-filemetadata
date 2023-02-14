ff10beaab8119f9ab01083c811f9279ec46c9d31
melany estevez camata <elibialany@gmail.com>
2023-02-14 13:16:02 -0400

    start project

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' })
 var app = express();
 
 app.use(cors());
@@ -11,8 +12,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), async (req, res) => {
+  try {
+    res.json({
+      "name": req.file.originalname,
+      "type": req.file.mimetype,
+      "size": req.file.size
+    })
+  } catch (error) {
+    res.json(error)
+  }
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
